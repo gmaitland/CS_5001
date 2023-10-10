a04591a190310306cf749f6dccf39d0a8b6761a4
Ivy <garfieldivymaitland@gmail.com>
2023-10-10 11:35:45 -0400

    inserted heading

--- Tutoring/nested_nested_list_.py
+++ Tutoring/nested_nested_list_.py
@@ -1,25 +1,32 @@
+"""
+    Garfield Maitland
+    CS 5001
+    10/10/2023
+    Tutoring - nested_nested_while_loops.py
+"""
+
 
 def main():
     # print out every single element in the list
     list1 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
 
-    i = 0 # reset i
+    i = 0  # reset i
     # iterate through the first level of multidimensional list
     while (i < len(list1)):
         print(f'\n\ntop level: {list1}')
         print('\nnext row')
-        print(f'i: {i}') # print out row index
+        print(f'i: {i}')  # print out row index
         print(f'list[i] = {list1[i]}')
 
-        j = 0 # rest j
-        while (j < len(list1[i])): # 2 not < 2
+        j = 0  # rest j
+        while (j < len(list1[i])):  # 2 not < 2
             print('inner loop')
             # print out "column" index (elements of a row)
-            print(f'j: {j}')
+            print(f'j: {j}'
             print(f'list[{i}][j] = {list1[i][j]}')
 
-            k = 0 # reset k
-            while (k < len(list1[i][j])): # 2 not < 2
+            k = 0  # reset k
+            while (k < len(list1[i][j])):  # 2 not < 2
                 print('inner inner loop')
                 # print out "column", "column" index (elements of a row)
                 print(f'k: {k}')
@@ -29,4 +36,5 @@ def main():
             j += 1
         i += 1
 
-main ()
+
+main()
