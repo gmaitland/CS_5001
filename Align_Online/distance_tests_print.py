c37aee1376132ed9104c762a38ff74cdbad3add0
Ivy <garfieldivymaitland@gmail.com>
2023-10-22 15:37:42 -0400

    Implementing the distance_tests_print.py file

---
+++ Align_Online/distance_tests_print.py
@@ -0,0 +1,11 @@
+"""
+    CS 5001
+    10/22/2023
+    Align Online Module 2 - distance_tests_print.py
+    Garfield Maitland
+"""
+
+"""
+    Practice with return statements and convert,
+    Tugboat's weight from stone to pounds.
+"""
