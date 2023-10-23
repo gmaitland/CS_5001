588bd7d89a5fe8d461aa539b51c84c44c32aa135
Ivy <garfieldivymaitland@gmail.com>
2023-10-23 15:32:01 -0400

    Learned about the 'off by 1 error' and about the possibility to use the debugger

--- Align_Online/Module_3/beetlejuice.py
+++ Align_Online/Module_3/beetlejuice.py
@@ -1,7 +1,17 @@
-''' Align Online
-    CS5001
-    Sample code -- a simple while loop
-'''
+"""
+    CS 5001
+    10/23/2023
+    Align Online Module 3 - beetlejuice.py
+    Garfield Maitland
+"""
+
+# Learned about the infamous 'off by 1 error'
+
+"""
+    Created a while loop and learned how to debug it,
+    by utilizing print statements. Learned about
+    breakpoints and the debugger.
+"""
 
 
 def main():
@@ -12,3 +22,6 @@ def main():
 
 
 main()
+
+""""
+    We initialized our variable i, then we """
