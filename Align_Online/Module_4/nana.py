5f124d4ba90b96c90d9cdcca44b585eb41413126
Ivy <garfieldivymaitland@gmail.com>
2023-10-23 19:05:16 -0400

    Deconstructed the while loop. And understood how it functions

--- Align_Online/Module_4/nana.py
+++ Align_Online/Module_4/nana.py
@@ -1,13 +1,24 @@
-''' Align Online
-    Sample code -- A simple for loop
-'''
+"""
+    CS 5001
+    10/23/2023
+    Align Online Module 4 - nana.py
+    Garfield Maitland
+"""
 
+"""
+    We are printing a for loop that will range
+    over all the iterations of the function.
+    After looking at the function in more detail,
+    it appears that it may not run. Wow, it ran,
+    this will require further investigation, and
+    exploration
+"""
 
 def main():
     for i in range(8):
         print("na", end=" ")
         print(i)
-    print("hey hey goodbye")
+    print("hey hey hey goodbye")
     print(i)
 
 
