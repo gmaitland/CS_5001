4ff12ec20146004596f64776002288fc17cfa393
Ivy <garfieldivymaitland@gmail.com>
2023-10-27 17:36:00 -0400

    Created multiple for loops. Intialized a variable, and created a conditional statement, and created an update statement

--- Align_Online/Module_5/min_temp.py
+++ Align_Online/Module_5/min_temp.py
@@ -11,6 +11,8 @@ HIGH_TEMPS = [92, 84, 98, 87, 94, 86, 75, 81, 91, 96, 75,
 
 
 def main():
+
+    """
     # min high temp in July
     min_temp = 100
     for temp in HIGH_TEMPS:
@@ -18,6 +20,30 @@ def main():
             min_temp = temp
     print("Min temp in July was", min_temp, "degrees!")
     print(min_temp)
+    """
+
+max_temp = 80
+for temp in HIGH_TEMPS:
+    if temp > max_temp:
+        max_temp = temp
+        print(max_temp)
+
+min_temp = 80
+for temp in HIGH_TEMPS:
+    if temp < min_temp:
+        min_temp = temp
+        print(min_temp)
+
+average_temp = int(sum(HIGH_TEMPS)) / int(len(HIGH_TEMPS))
+print(average_temp)
+
+j = 0
+for i in HIGH_TEMPS:
+    j += i
+print(f'The arithmetic total is {j}')
+a = j / len(HIGH_TEMPS)
+print(f'The average temperature is {int(a)}')
+
 
 
 main()
