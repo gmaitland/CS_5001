09b7826117f2ca74a27862236779a9b0ae8a9100
Ivy <garfieldivymaitland@gmail.com>
2023-10-28 11:37:51 -0400

    Pizzas

--- Align_Online/Module_6/pizza.py
+++ Align_Online/Module_6/pizza.py
@@ -9,20 +9,20 @@
 Module: Handing Exceptions
 
 This is the code that we are starting with in module 10.  Many changes 
-will be made to the code and there will be a couple of different version
+will be made to the code and there will be a couple of different versions
 as we cover the material in this module.
 """
 
 
 def get_slices(pies, folks):
-    '''
+    """
         Function: get_slices that calculates the number of slices of pizza
         Params:   pies, the number of pies
                   folks, the number of people
         Returns:  the number of slices per person
-    '''
+    """
     if folks != 0:
-        slices = pies * int(8*2) // folks
+        slices = pies * 8 // folks
         return slices
 
 
