375df9d8fcf691d844a97d3736fb62f825e48c29
Ivy <garfieldivymaitland@gmail.com>
2023-10-28 09:43:55 -0400

    Practiced for while and for loops. Designed it in my mind first, and then wrote it on epaper, and then wrote it on an emonitor, and then ran it.

--- Align_Online/Module_5/vertical_print.py
+++ Align_Online/Module_5/vertical_print.py
@@ -36,9 +36,37 @@ for num in lst:
 
 
 # s = "abcde"
+s = input("Please enter a word:")
 t = 0
 while t < len(s):
     print(s[t])
     t += 1
+
+# Reviewed while and for loops from yesterday
+
+pets = ["bird", "cat", "dog", "fish"]
+i = 0
+while i < len(pets):
+    print(pets[i])
+
+    j = 0
+    while j < len(pets[i]):
+        print(pets[i][j])
+        j += 1
+    i += 1
+
+
+a = "Road"
+for letter in a:
+    print(letter)
+
+"""
+    While loops typically have 5 lines of code. The sequence/list/array/string
+    to be iterated/looped over, the variable index value that will be updated,
+    the conditional statement that is designed to be false and using methods
+    like .len(), the print() statement to print a value to the console, and
+    finally the update statement to increment the index variable value, so that
+    we can iterate to the next element within the list.
+"""
 
 main()
