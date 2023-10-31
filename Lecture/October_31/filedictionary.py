882af9cc57db1722c279c632ea8e0b25afce970d
Ivy <garfieldivymaitland@gmail.com>
2023-10-31 19:59:58 -0400

    Externally called the words.txt file

--- Lecture/October_31/filedictionary.py
+++ Lecture/October_31/filedictionary.py
@@ -9,9 +9,25 @@ def load_dictionary(file_name):
     with open(file_name, mode="r") as in_file:
         logos = {}
 
-        for each in_file:
+        for each in in_file:
             each = each.strip("\n")
             english, french, spanish = each.split(",")
             logos[english] = [french, spanish]
 
         return logos
+
+def test_file_dictionary():
+    translate = load_dictionary("words.txt")
+    word = input("Enter a word: ")
+    word = word.lower()
+    if word in translate:
+        print(f"Translated word is: {translate[word][0]} in French")
+        print(f"Translated word is: {translate[word][1]} in Spanish")
+    else:
+        print(f"Cannot translate {word}")
+
+def main():
+    test_file_dictionary()
+
+if __name__ == "__main__":
+    main()
