59bec70de1ecdac077e984c971f210912a8c6505
Ivy <garfieldivymaitland@gmail.com>
2023-11-06 19:23:45 -0500

    Still working on emojini.py. it appears that the mappings are not mapping to the dictionary

--- Homework_6/emojini.py
+++ Homework_6/emojini.py
@@ -1,75 +1,77 @@
-"""
-    CS 5001
-    11/06/2023
-    Homework 6 - emojini.py
-    Garfield Maitland
-"""
-
-
-def read_emoji_file(file_name):
-    with open(file_name, 'r') as file:
-        lines = file.readlines()
-
-    metadata = lines[0].strip().split()
-    emoji_dict = {metadata[i]: {} for i in range(1, len(metadata))}
-    for line in lines[1:]:
-        parts = line.strip().split()
-        for i in range(1, len(metadata)):
-            emoji_dict[metadata[i]][parts[0]] = parts[i]
+def read_emoji_file(file_name: str):
+    emoji_dict = {'english_to_western': {},
+                  'english_to_kaomoji': {},
+                  'kaomoji_to_english': {},
+                  'kaomoji_to_western': {},
+                  'western_to_english': {},
+                  'western_to_kaomoji': {},
+                  }
+    with open(file_name, 'r', encoding='utf-8') as file:
+        next(file)  # Skip the metadata header
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) == 4:
+                emoji_dict['english_to_western'][parts[1].lower()] = parts[2]
+                emoji_dict['english_to_kaomoji'][parts[1].lower()] = parts[3]
+                emoji_dict['kaomoji_to_english'][parts[3]] = parts[1]
+                emoji_dict['western_to_english'][parts[2]] = parts[1]
     return emoji_dict
 
+def parse_directives_file(file_name: str):
+    instructions = []
+    with open(file_name, 'r', encoding='utf-8') as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) == 4:
+                instruction = {
+                    'mode': f"{parts[0]}_to_{parts[1]}",
+                    'source_file': parts[2],
+                    'output_file': parts[3]
+                }
+                instructions.append(instruction)
+    return instructions
 
-def translate_line(line, word_dict):
-    translated_line = ''
-    word_start = 0
+def translate_text(source_text: str, emoji_dict: dict, mode: str):
+    words = source_text.split()
+    translated_words = []
 
-    for i, char in enumerate(line):
-        if char in ' \n\t.,!?()"-':
-            if word_start < i:
-                word = line[word_start:i]
-                translated_line += word_dict.get(word, word)
-            translated_line += char
-            word_start = i + 1
+    for word in words:
+        word_cleaned, prefix, suffix = strip_punctuation(word)
+        if word_cleaned.lower() in emoji_dict[mode]:
+            translated_word = prefix + emoji_dict[mode][word_cleaned.lower()] + suffix
+        else:
+            translated_word = word
+        translated_words.append(translated_word)
 
-    # Add the last word if the line doesn't end with a punctuation mark
-    if word_start < len(line):
-        word = line[word_start:]
-        translated_line += word_dict.get(word, word)
+    return ' '.join(translated_words)
 
-    return translated_line
+def strip_punctuation(word: str):
+    prefix, suffix = '', ''
+    while word and not word[0].isalnum():
+        prefix += word[0]
+        word = word[1:]
+    while word and not word[-1].isalnum():
+        suffix = word[-1] + suffix
+        word = word[:-1]
+    return word, prefix, suffix
 
+def write_to_file(translated_text: str, file_name: str):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        file.write(translated_text)
 
-def batch_translate(emoji_file_name, directives_file_name):
+def batch_translate(emoji_file_name: str, directives_file_name: str):
     emoji_dict = read_emoji_file(emoji_file_name)
+    translation_instructions = parse_directives_file(directives_file_name)
 
-    try:
-        with open(directives_file_name, 'r') as directives_file:
-            for line in directives_file:
-                source_lang, target_lang, source_file_name, target_file_name = line.strip().split()
+    for instruction in translation_instructions:
+        with open(instruction['source_file'], 'r', encoding='utf-8') as file:
+            source_text = file.read()
 
-                # Depending on the direction of translation, the word dictionary may differ
-                if source_lang != 'english':
-                    word_dict = {v: k for k, v in emoji_dict[source_lang].items()}
-                else:
-                    word_dict = emoji_dict[target_lang]
+        translated_text = translate_text(source_text, emoji_dict, instruction['mode'])
+        write_to_file(translated_text, instruction['output_file'])
+        print(f"Processed {instruction['source_file']}: {instruction['mode'].replace(' ', ' -> ')}")
 
-                try:
-                    with open(source_file_name, 'r') as source_file, open(target_file_name, 'w') as target_file:
-                        for line in source_file:
-                            translated_line = translate_line(line, word_dict)
-                            target_file.write(translated_line)
-                except FileNotFoundError:
-                    print(f"Error: The file {source_file_name} does not exist.")
-                except Exception as e:
-                    print(f"An error occurred while processing the file {source_file_name}: {e}")
+    print("All files processed.")
 
-                print(f"Processing {source_file_name}: {source_lang} -> {target_lang}")
-    except FileNotFoundError:
-        print(f"Error: The directives file {directives_file_name} does not exist.")
-    except Exception as e:
-        print(f"An error occurred while reading the directives file: {e}")
-
-    print("done")
-
-# To use the function, simply call it with the names of your emoji and directives files:
-# batch_translate('emojis.txt', 'emoji_directives.txt')
+if __name__ == "__main__":
+    batch_translate("emojis.txt", "emoji_directives.txt")
