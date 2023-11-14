d5e3f25e940192dd0b7b96fbdd32d081c59ed1e4
Ivy <garfieldivymaitland@gmail.com>
2023-11-14 17:31:00 -0500

    Started tkintering with the tkinter module

--- Lecture/November_11/tkinter_practice.py
+++ Lecture/November_11/tkinter_practice.py
@@ -7,6 +7,7 @@
 
 import tkinter
 
+"""
 window = tkinter.Tk()
 data = tkinter.StringVar()
 data.set('Data to display '
@@ -14,7 +15,18 @@ data.set('Data to display '
 # data.set('Show your work')
 label = tkinter.Label(window, textvariable=data)
 label.pack()
+window.mainloop()
+"""
 
+window = tkinter.Tk()
+frame = tkinter.Frame(window)
+frame.pack()
+first = tkinter.Label(frame, text='First label')
+first.pack()
+second = tkinter.Label(frame, text='Second label')
+second.pack()
+third = tkinter.Label(frame, text='Third label')
+third.pack()
 window.mainloop()
 
 # Looks like we are using setter and getter functions again
