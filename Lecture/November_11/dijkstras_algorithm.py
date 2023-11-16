0d942353ea0c6d369ed9ba070edc64578a926a3b
Ivy <garfieldivymaitland@gmail.com>
2023-11-15 23:18:20 -0500

    Started the implementation of dijkstra's algorithm

---
+++ Lecture/November_11/dijkstras_algorithm.py
@@ -0,0 +1,16 @@
+"""
+    Garfield Maitland
+    CS 5001
+    11/15/2023
+    Practice - dijkstras_algorithm.py
+"""
+
+graphs = {
+    's':{'a':8, 'b':4},
+    'a':{'b':4},
+    'b':{'a':3,'c':2, 'd':5},
+    'c':{'d':2},
+    'd':{}
+}
+
+
