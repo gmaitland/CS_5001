2642904cf49bda3c281246f72baa8acfe8ab3e6b
Ivy <garfieldivymaitland@gmail.com>
2023-11-19 21:39:40 -0500

    Practice with sets

---
+++ Lecture/November_11/practice_with_sets.py
@@ -0,0 +1,21 @@
+"""
+    Garfield Maitland
+    CS 5001
+    11/19/2023
+    Practice - practice_with_sets.py
+"""
+
+"""
+    Data compiled from the python docs page:
+    
+    A set is an unordered collection with no duplicate elements
+    Basic uses include membership test and eliminating duplicate
+    entries. Set objects also support mathematical operations like
+    union, intersection, difference, and symmetric difference.
+    
+    Curly braces or the set() built in function can used to create sets.
+    To create an empty set, you have to use set(). This is because 
+    {} creates an empty dictionary, not an empty set.
+    
+    Let'
+"""
