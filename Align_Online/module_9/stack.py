29c9bcca0e8dc5eabe89db5dce0c9ef540a7a59d
Ivy <garfieldivymaitland@gmail.com>
2023-11-21 16:09:08 -0500

    Need to check the implementation of main()

--- Align_Online/module_9/stack.py
+++ Align_Online/module_9/stack.py
@@ -26,11 +26,16 @@ class Stack:
     def is_empty(self):
         return not self.mystack
 
-    def peek(self): # aka top()
+    def peek(self):  # aka top()
         if self.mystack:
-            return self.mystack[-1] 
+            return self.mystack[-1]
         return None
+
 
 def main():
     # mystack =
     print("Need to check the implementation later")
+
+
+if __name__ == "__main__":
+    main()
