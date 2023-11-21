c45e5bb940ccd04935b1e8e709ed386393e064f0
Ivy <garfieldivymaitland@gmail.com>
2023-11-20 20:02:18 -0500

    Practice with creating dictionaries

---
+++ Lecture/November_11/practice_with_dictionaries.py
@@ -0,0 +1,12 @@
+"""
+    Garfield Maitland
+    CS 5001
+    11/19/2023
+    Practice - practice_with_dictionaries.py
+"""
+
+"""
+    Dictionaries are sometimes called associative memories. Associative arrays
+"""
+
+tel = {'jack' : 4098, 'sape' : 4139}
