356ac6bc4abcbc25fe9c0907fc78db720cdeb7e8
Ivy <garfieldivymaitland@gmail.com>
2023-11-25 14:41:25 -0500

    Refactored the main

--- Align_Online/Module_8/recursion_practice.py
+++ Align_Online/Module_8/recursion_practice.py
@@ -20,19 +20,7 @@ def mystery(n):
 
 
 def main():
-    """
-    print(mystery(-1))
-    print(mystery(0))
-    print(mystery(1))
-    print(mystery(2))
-    print(mystery(3))
-    """
-    print(mystery(4))
-    print(mystery(77))
-    print(mystery(100))
-    print(mystery(7))
-    print(mystery(12))
-
+    print(mystery(10))
 
 if __name__ == "__main__":
     main()
